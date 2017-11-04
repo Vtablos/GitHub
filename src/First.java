bb1ab4889c1b8dd55197908c0c53f3512f32f24c
Alex Soft <alianavi83@gmail.com>
2017-11-04 18:39:44 +0300

    'Second'

--- src/First.java
+++ src/First.java
@@ -1,5 +1,10 @@
 public class First {
 
-    int a = 5;
-    String b= "first";
+    String a = "git init";
+    String b= "git remote add origin URL";
+    String c= "git branch --set-upstream-to=origin/master  master";
+    String history = "git pull origin branchname --allow-unrelated-histories";
+    String pull = "git pull";
+    String add = "git add";
+    String comitt = "git comit -m ' ";
 }
